f63da15334f4fb0cb884017ffc285e4125a02565
EdwardQuevedoP <edwardquevedop@gmail.com>
2023-11-23 22:44:13 -0500

    se comento el detenv

--- hoja1.js
+++ hoja1.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const { Pool } = require('pg');
 const app = express()
-const port = 3000;
+const port = process.env.PORT||3000;
 app.use(express.json())
-require('dotenv').config()
+// require('dotenv').config()
 const swaggerUi = require('swagger-ui-express');
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup());
 const pool = new Pool({
