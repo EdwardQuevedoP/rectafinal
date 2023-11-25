e61db4f12bde499e8b2f837f386845688ed0d819
EdwardQuevedoP <edwardquevedop@gmail.com>
2023-11-24 23:51:13 -0500

    hoja nueva2

--- hoja1.js
+++ hoja1.js
@@ -2,10 +2,15 @@ const express = require('express');
 const { Pool } = require('pg');
 const app = express()
 const port = process.env.PORT||3000;
-app.use(express.json())
-require('dotenv').config()
+
 const swaggerUi = require('swagger-ui-express');
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup());
+
+const swaggerJsdoc = require('swagger-jsdoc');
+
+app.use(express.json())
+
+require('dotenv').config()
+
 const pool = new Pool({
     user: 'default',
     host: 'ep-nameless-thunder-96715843-pooler.us-east-1.postgres.vercel-storage.com',
@@ -14,8 +19,37 @@ const pool = new Pool({
     port: 5432,
     ssl: {rejectUnauthorized: false}
 });
+// Definir la configuración de Swagger
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Mi API',
+      version: '1.0.0',
+    },
+  },
+  apis: ['hoja1.js'], // Ruta al archivo que contiene las rutas de tu API
+};
 
+const specs = swaggerJsdoc(options);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+
+// Definir tus rutas aquí
+app.get('/', (req, res) => {
+  res.send('Hola, mundo!');
+});
+
+/**
+ * @swagger
+ * /:
+ *   get:
+ *     summary: Retorna un saludo
+ *     responses:
+ *       200:
+ *         description: Respuesta exitosa
+ */
 const API_KEY = process.env.API_KEY;
+
 const apiKEYvalidations = (req,res, next) =>{
     const userapiKey = req.get('x-api-key')
     if(userapiKey && userapiKey === API_KEY){
